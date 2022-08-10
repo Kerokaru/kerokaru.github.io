61a1904b1db718498df20c935653b491411f3701
0xKero <s.kerokaru@gmail.com>
2022-08-10 17:13:11 +0200

    Update

--- button.js
+++ button.js
@@ -1,6 +1,7 @@
 
 function nomAleatoire {
-    document.querySelector("h2").innerHTML = "crotte";
+    document.getElementById("crystalname").innerHTML = "crotte";
 }
 
 document.getElementById("bouton1").addEventListener("click", nomAleatoire);
+
